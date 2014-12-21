d10cb19b7115b7073d33a8f05b98d846776c7e02
clemansles <clemansles@gmail.com>
2014-12-21 19:44:15 +0100

    implementatio oauth

--- feedlocal.js
+++ feedlocal.js
@@ -1,1 +1,21 @@
-/home/averroes/feedlocal.js
+var express =require('express'),
+    bodyParser=require('body-parser'),
+    oauthserver = require('node-oauth-server');
+
+var app = express();
+
+app.oauth = oauthserver({
+  model: {}, // See below for specification
+  grants: ['password'],
+  debug: true
+});
+
+app.all('/oauth/token', app.oauth.grant());
+
+app.get('/', app.oauth.authorise(), function (req, res) {
+  res.send('Secret area');
+});
+
+app.use(app.oauth.errorHandler());
+
+app.listen(5000);
